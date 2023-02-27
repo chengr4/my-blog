9da728b9037aac84898e8708cea18042722e5e04
Eddie Cheng <eddie@vyncahealth.com>
2023-02-27 15:24:20 +0900

    udpate header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,23 +4,29 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className='container mx-auto px-10 mb-10 bg-white'>
-      <div className='w-full inline-block'>
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block py-8'>
         <div className='md:float-left block'>
           <Link href='/'>
-            <span className='cursor-pointer font-bold text-4xl'>
-              Eddie's Blog
+            <span className='cursor-pointer font-bold text-4xl text-white'>
+              {"Eddie's Blog"}
             </span>
           </Link>
         </div>
-        <div className='hidden md:contents'>
-          <span className='mt-2 float-right align-middle ml-4'>
-            teffffff
+        <div className='hidden md:float-left md:contents'>
+          <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+            About
+          </span>
+          <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+            Other
+          </span>
+          <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+            CS
+          </span>
+          <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+            Web
           </span>
         </div>
-      </div>
-      <div className='border-b w-full inline-block'>
-
       </div>
     </div>
   )
