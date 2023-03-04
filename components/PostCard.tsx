a73bb2b2c21048050824f7a96cc6338eb66724df
Eddie Cheng <eddie@vyncahealth.com>
2023-03-04 22:24:47 +0900

    use real data

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -4,20 +4,20 @@ import Link from 'next/link';
 interface PostCardProps {
   post: {
     title: string
-    des: string
+    excerpt: string
+    file: string
+    date: string
   }
 };
 
 const PostCard = ({ post }: PostCardProps) => {
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
-      {post.title}
-      {post.des}
-      <Link href={'/posts/contents/tt'} >
+      <Link href={`/post/${post.file}`} >
         <h1 className='transition duration-700 mb-8 cursor:pointer text-center hover:text-pink-600 text-3xl font-semibold'>
-          test
+          {post.title}
         </h1>
-        <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>EXCERPT</p>
+        <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
       </Link>
       <div className='block lg:flex text-center item-center justify-center mb-8 w-full'>
         {/* Post Date */}
@@ -25,7 +25,7 @@ const PostCard = ({ post }: PostCardProps) => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
           </svg>
-          <span className="align-middle">1234-67-40</span>
+          <span className="align-middle">{post.date}</span>
         </div>
       </div>
     </div>
