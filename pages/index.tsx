9df5322b355e4f176f2ef8dacfef331c17163b22
Eddie Cheng <schaffenneu47@gmail.com>
2023-03-24 20:54:40 +0900

    use getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,49 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { PostCard, PostWidget } from '../components';
-import { uniqueId } from 'lodash';
-import { useEffect, useState } from 'react';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { PostCard, PostWidget } from "../components";
+import { uniqueId } from "lodash";
 
+interface HomeProps {
+  posts: any[];
+}
 
-const Home: NextPage = () => {
-  const [posts, setPosts] = useState([]);
-
-  // fetch data from public/posts.json
-  useEffect(() => {
-    fetch('/posts/index.json')
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
-
-
+const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div className="container mx-auto items-center justify-center py-2 mb-8">
       <Head>
         <title>My Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
           {/* Posts */}
-          {posts.map((post, index) => ( <PostCard post={post} key={uniqueId('post_')} /> ))}
+          {posts.map((post: any) => (
+            <PostCard post={post} key={uniqueId("post_")} />
+          ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'>
-              {/* Show recent posts */}
-              <PostWidget />
-              {/* Still thinking how to implement Category */}
-              {/* <Category /> */}
-            </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            {/* Show recent posts */}
+            <PostWidget />
+            {/* Still thinking how to implement Category */}
+            {/* <Category /> */}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  // Fetch data from an API or database
+  const res = await fetch('https://raw.githubusercontent.com/chengr4/my-blog/main/data/posts/index.json');
+  const data = await res.json();
+
+  return {
+    props: {
+      posts: data
+    }
+  };
 }
 
 export default Home;
