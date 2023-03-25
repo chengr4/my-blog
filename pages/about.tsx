83f9c08327354efeab8d0fd273431fb40c6099c2
Eddie Cheng <schaffenneu47@gmail.com>
2023-03-25 21:29:39 +0900

    add about page

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="container mx-auto items-center justify-center py-2 mb-8">
+      <div className="cursor-pointer font-bold text-4xl text-white">
+        To Be Continued...
+      </div>
+    </div>
+  );
+}
