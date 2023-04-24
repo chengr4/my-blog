f0051ebb48a49a9d4cc40bede82c802c9b859225
Eddie Cheng <shaffenneu47@gmail.com>
2023-04-24 15:02:44 +0800

    sort posts before return them

--- services/getAllPosts.ts
+++ services/getAllPosts.ts
@@ -1,6 +1,16 @@
+import { Post } from "../types/global";
+
+// get all posts and sort them by editedDate and createdDate
 export default async function getAllPosts() {
   const res = await fetch('https://raw.githubusercontent.com/chengr4/my-blog/main/data/posts/index.json');
-  const data = await res.json();
+  const data: Post[] = await res.json();
+
+  // sort posts by editedDate and createdDate
+  data.sort((a, b) => {
+    const aDate = new Date(a.editedDate || a.createdDate);
+    const bDate = new Date(b.editedDate || b.createdDate);
+    return bDate.getTime() - aDate.getTime();
+  });
 
   return data;
 }
